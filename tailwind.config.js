ee2ac95bf7cc1d8cb9b65ab631105896c97ee4da
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-05-18 18:50:32 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backgroundImage: theme => ({
+        'gradiente': "linear-gradient(135deg, #283acb 0%, #8650c8 50%, #de0a0a 100%)"
+      }),
+    color: {
+      'render': "#283acb"
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
