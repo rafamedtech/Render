23cca9313fc9377b72ca41ecc6a283400fbad00a
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-08-16 17:43:17 -0700

    form sheets test

--- public/script.js
+++ public/script.js
@@ -91,3 +91,15 @@ function phoneValidation() {
     return false;
   }
 }
+
+// Send form to Google Sheets
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbxZBDBlx1_NR_u1daUZyw6LaFmuIA0sjlU9WYYOizKKlnGvA8xWcfsAbrkbVSqNSpTB5Q/exec';
+const form = document.forms['google-sheet'];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => alert('You have successfully submitted.'))
+    .catch((error) => console.error('Error!', error.message));
+});
