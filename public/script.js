589d6745e8ffdc47378f939f514b0587933d6d29
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-08-19 11:50:41 -0700

    form test 2

--- public/script.js
+++ public/script.js
@@ -66,7 +66,7 @@ form.addEventListener('submit', (e) => {
     console.log('esto tambien funciono');
     modal.classList.remove('hidden-modal');
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-      .then((response) => location.assign('/'))
+      .then((response) => location.assign('/index.html'))
       .catch((error) => console.error('Error!', error.message));
   }
 });
